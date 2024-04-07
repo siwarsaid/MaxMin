4c189169435e78495aca8ff36a6e3aa022937f8a
siwar said <siwarsaid8@gmail.com>
2024-04-07 23:28:00 +0200

    quete 1 fait

---
+++ MaxMin/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbminsaisie;
+            int nbmaxsaisie;
+            int entier;
+
+            Console.WriteLine("Limit1:");
+            nbminsaisie = Int32.Parse(Console.ReadLine());
+          
+
+             Console.WriteLine("Limit2:");
+            nbmaxsaisie = Int32.Parse(Console.ReadLine());
+          
+
+            Console.WriteLine("Entier:");
+              entier = Int32.Parse(Console.ReadLine());
+            if (nbmaxsaisie < entier)
+            {
+             
+                Console.WriteLine("Vous avez saisi " +entier+ " qui est supérieur à " + nbmaxsaisie + " qui est le maximum.");
+                Console.WriteLine("Nombre entier résultant :" + nbmaxsaisie);
+            }
+        }
+    }
+}
